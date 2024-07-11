db1963af239c7308c87332b153bdfb04b910b95e
ShadrachKadah <shadrachaudukadah1@gmail.com>
2024-07-11 16:34:29 -0700

    0. Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
-"""
-Minimum operations
-"""
+""" Minimum Operations
+    """
 
-def min_operations(n: int) -> int:
-    """ Minimum Operations needed to get n S characters """
-    if n < 2:
-        return 0
 
-    next_copy = 'S'
-    body = 'S'
-    operations = 0
-
-    while len(body) < n:
+def minOperations(n: int) -> int:
+    """ Minimum Operations needed to get n H characters """
+    next = 'H'
+    body = 'H'
+    op = 0
+    while (len(body) < n):
         if n % len(body) == 0:
-            next_copy = body
-            operations += 2
+            op += 2
+            next = body
             body += body
         else:
-            body += next_copy
-            operations += 1
-
-    return operations
+            op += 1
+            body += next
+    if len(body) != n:
+        return 0
+    return op
